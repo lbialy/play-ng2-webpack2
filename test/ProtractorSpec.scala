7e3f3973cb5d6896fcf20fc9fe06fb3107c656bf
Łukasz Biały <GJJ7@global.tesco.org>
2017-02-05 16:11:09 +0100

    update ProtractorSpec to run angular-cli e2e tests

--- test/ProtractorSpec.scala
+++ test/ProtractorSpec.scala
@@ -2,45 +2,35 @@ import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.Await
-import scala.sys.process.{Process, ProcessIO}
-import scala.io.Source._
 import scala.concurrent.duration._
+import scala.io.Source._
+import scala.sys.process.{Process, ProcessIO}
 
 class ProtractorSpec extends PlaySpec with OneServerPerSuite {
 
   "my application" should {
 
     "pass the protractor tests" in {
-      val baseUrl = s"http://localhost:$port"
+      val baseUrl = s"http://localhost:$port/"
       val wsClient = app.injector.instanceOf[WSClient]
 
       val resp = Await.result(wsClient.url(baseUrl).get(), 2.seconds)
-      resp.status === 200
+      resp.status must be(200)
 
-      println(s"Executing protractor using command: 'node_modules/protractor/bin/protractor --baseUrl=$baseUrl")
+      println(s"Executing protractor using command: 'npm run e2e -- --baseUrl $baseUrl")
 
-      runProtractorTests(baseUrl) === 0
+      runProtractorTests(baseUrl) must be(0)
     }
 
   }
 
-  private def compileE2Etests: Int = {
-    Process("tsc" :: "-p" :: "e2e" :: Nil, app.getFile("ui"))
-      .run(usingProcessIO)
+  private def runProtractorTests(baseUrl: String): Int = {
+    Process(s"npm run e2e -- --baseUrl $baseUrl", app.getFile("ui"))
+      .run(pipingInputAndOutput)
       .exitValue()
   }
 
-  private def runProtractorTests(baseUrl: String): Int = {
-    val e2eTestsCompilationResult = compileE2Etests
-
-    if (e2eTestsCompilationResult === 0) {
-      Process("node_modules/protractor/bin/protractor" :: s"--baseUrl=$baseUrl" :: Nil, app.getFile("ui"))
-        .run(usingProcessIO)
-        .exitValue()
-    } else e2eTestsCompilationResult
-  }
-
-  private def usingProcessIO = new ProcessIO(_ => (),
+  private def pipingInputAndOutput = new ProcessIO(_ => (),
     stdout => fromInputStream(stdout).getLines().foreach(println),
     stderr => fromInputStream(stderr).getLines().foreach(println)
   )
